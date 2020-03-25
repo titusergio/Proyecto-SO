a63274659283ce98d9796768678734e245648ce5
David Sanchez <davidsanchzv@gmail.com>
2020-03-25 21:17:34 +0100

    v1

--- basededatos.sql
+++ basededatos.sql
@@ -2,13 +2,13 @@ DROP DATABASE IF EXISTS juego;
 CREATE DATABASE juego;
 USE juego;
 CREATE TABLE jugador (
-   username TEXT NOT NULL,
+   usuario VARCHAR(16) PRIMARY KEY NOT NULL,
    contrasena  INTEGER NOT NULL,
    email TEXT NOT NULL
 )ENGINE = InnoDB;
 
 CREATE TABLE partida (
-   ID INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+   ID INTEGER PRIMARY KEY  NOT NULL AUTO_INCREMENT,
    fecha DATE NOT NULL,
    duracion INTEGER NOT NULL,
    ganador TEXT NOT NULL
@@ -17,9 +17,9 @@ CREATE TABLE partida (
 CREATE TABLE resumen (
    posicion INTEGER NOT NULL,
    puntuacion INTEGER NOT NULL,
-   jugador TEXT,
-   partida INTEGER,
-   FOREIGN KEY (jugador) REFERENCES jugador(username),
+   jugador VARCHAR(16) NOT NULL,
+   partida INTEGER NOT NULL,
+   FOREIGN KEY (jugador) REFERENCES jugador(usuario),
    FOREIGN KEY (partida) REFERENCES partida(ID)
 )ENGINE = InnoDB;
 
@@ -27,10 +27,10 @@ INSERT INTO jugador VALUES('davidsv', 1234, 'david@upc.edu');
 INSERT INTO jugador VALUES('sergiogl', 1234, 'sergio@upc.edu');
 INSERT INTO jugador VALUES('martigc', 1234, 'marti@upc.edu');
 
-INSERT INTO partida VALUES(1, 2020-01-12, 37, 'davidsv');
-INSERT INTO partida VALUES(2, 2020-01-18, 57, 'sergiogl');
-INSERT INTO partida VALUES(3, 2020-02-15, 47, 'martigc');
-INSERT INTO partida VALUES(NULL, 2020-01-18, 43, 'martigc');
+INSERT INTO partida VALUES(1, '2020-01-12', 37, 'davidsv');
+INSERT INTO partida VALUES(2, '2020-01-18', 57, 'sergiogl');
+INSERT INTO partida VALUES(3, '2020-02-15', 47, 'martigc');
+INSERT INTO partida VALUES(NULL, '2020-01-18', 43, 'martigc');
 
 INSERT INTO resumen VALUES(1, 12, 'davidsv', 1);
 INSERT INTO resumen VALUES(2, 10, 'martigc', 3);
